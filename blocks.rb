1b789fba03e4c0a5cbf52524bffceed49b6ebb32
Louie-cpu <luis.u.m779@gmail.com>
2024-05-30 21:41:15 -0700

    fixing methods

--- blocks.rb
+++ blocks.rb
@@ -10,20 +10,36 @@ numbers = (1..20).to_a.reverse
 even_nums = numbers.select { |number| number.even? }
 p even_nums
 # Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
-nums = [11, 8, 22, 33, 5, 30, 19]
-nums.each do |value|
-  p value * 5
+def multiply_and_print(array)
+  array.each do |value|
+    p value * 5
+  end
 end
+nums = [11, 8, 22, 33, 5, 30, 19]
+multiply_and_print(nums)
 # Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+def capitalize_words(words)
+  words.map { |word| word.upcase }
+end
 words = ["sand", "pipe", "kettle", "practice", "jet"]
-word_cap = words.map { |str| str.upcase }
-
+word_cap = capitalize_words(words)
 p word_cap
-
-
-# Create a method that takes in an array of numbers and returns the largest number in the array.
-# Create a method that takes in an array of words and returns the word with the least number of characters.
+# # Create a method that takes in an array of numbers and returns the largest number in the array.
+def find_max(array)
+  array.max
+end
+array = [33, 14, 28, 11, 22, 8, 12, 19]
+nums_arr = find_max(array)
+p nums_arr
+# # Create a method that takes in an array of words and returns the word with the least number of characters.
+def shortest_word(words)
+  words.min_by { |word| word.length }
+end
+str_arr = ["sand", "pipe", "kettle", "practice", "jet"]
+least = shortest_word(str_arr)
+p least
 # Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
 # Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
 # Create a method that will take in a range of letters and return an array with all the letters capitalized.
